424acee3d1d51b620b26c0e3a1a37e1750e4cb4f
Masteryi <masteryi@MasteryideMacBook-Pro.local>
2017-06-15 17:06:41 +0800

    dafe

--- Classes/Category/UIKit/UILabel/UILabel+HXZExtension.m
+++ Classes/Category/UIKit/UILabel/UILabel+HXZExtension.m
@@ -8,9 +8,6 @@
 
 #import "UILabel+HXZExtension.h"
 
-#define SizeScale (([[UIScreen mainScreen] bounds].size.height > 568) ? [[UIScreen mainScreen] bounds].size.height/568 : 1)
-
-#define SmaleScale (([[UIScreen mainScreen] bounds].size.height == 568) ? 0.8 : 1)
 
 @implementation UILabel (HXZExtension)
 
@@ -31,41 +28,7 @@
 }
 
 
-+ (void)load{
-    //利用running time运行池的方法在程序启动的时候把两个方法替换 适用Xib建立的label
-    Method imp = class_getInstanceMethod([self class], @selector(initWithCoder:));
-    Method myImp = class_getInstanceMethod([self class], @selector(myInitWithCoder:));
-    method_exchangeImplementations(imp, myImp);  //交换方法
-    
-    Method fra = class_getInstanceMethod([self class], @selector(initWithFrame:));
-    Method myfra = class_getInstanceMethod([self class], @selector(myinitWithFrame:));
-    
-    method_exchangeImplementations(fra, myfra);  //交换方法
-}
 
 
-- (id)myInitWithCoder:(NSCoder*)aDecode{
-    [self myInitWithCoder:aDecode];
-    if (self) {
-        //部分不像改变字体的 把tag值设置成333跳过
-        if(self.tag != 333){
-            CGFloat fontSize = self.font.pointSize;
-            self.font = [UIFont systemFontOfSize:fontSize * SmaleScale];
-        }
-    }
-    return self;
-}
-
-- (instancetype)myinitWithFrame:(CGRect)frame{
-    [self myinitWithFrame:frame];
-    if (self) {
-        //部分不像改变字体的 把tag值设置成333跳过
-        if(self.tag != 333){
-            CGFloat fontSize = self.font.pointSize;
-            self.font = [UIFont systemFontOfSize:fontSize * SmaleScale];
-        }
-    }
-    return self;
-}
 
 @end
